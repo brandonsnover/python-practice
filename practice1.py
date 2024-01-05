51d80ff9a0f49646e3f75460c02c1d6f9a2df73b
brandonsnover <144728738+brandonsnover@users.noreply.github.com>
2024-01-05 00:51:03 -0600

    python practice

---
+++ practice1.py
@@ -0,0 +1,16 @@
+# Write a program that uses a variable to store a number, then prints that number plus 10.
+number = 2
+print(number + 10)
+
+# Write a program that uses a variable to store a word, then prints that word with all capital letters.
+word = "Hello"
+print(word.upper())
+
+# Write a program that uses variables to store two numbers, then prints the numbers added together.
+number = 3
+number2 = 6
+print(number + number2)
+
+# Write a program that uses a variable to store a word, then prints that word in reverse order.
+word = "Hello"
+print(word[::-1])
