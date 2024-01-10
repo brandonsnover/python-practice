cc373c37ef8a1a3c72122ad4ccb38831b4a89e8a
brandonsnover <144728738+brandonsnover@users.noreply.github.com>
2024-01-10 14:47:40 -0600

    python practice

--- practice21.py
+++ practice21.py
@@ -14,3 +14,74 @@ elif number > 10:
   print(1)
 else:
   print(0)
+
+# Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+number1 = 1
+number2 = 12
+if number1 < 10 and number2 < 10:
+  print(1)
+else:
+  print(0)
+
+# Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+number = 9001
+if number > 9000:
+  print(1)
+else:
+  print(-1)
+
+# Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+number = 7
+if number < 10:
+  print(9)
+elif number < 20:
+  print(19)
+elif number < 30:
+  print(29)
+else:
+  print(-1)
+
+# Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
+number1 = 7
+number2 = 24
+if number1 > 10 or number2 > 10:
+  print(100)
+else:
+  print(-100)
+
+# Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
+number = -4
+if number < 0:
+  print(1776)
+else:
+  print(1979)
+
+# Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+number = 99
+if number == 100:
+  print(100)
+elif number == 99:
+  print(99)
+else:
+  print(0)
+
+# Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
+number1 = 5
+number2 = 34
+if number1 < 0 and number2 > 0:
+  print(1)
+else: 
+  print(0)
+
+# Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
+number = 72
+if number > 80:
+  print(5)
+elif number > 60:
+  print(4)
+elif number > 40:
+  print(3)
+elif number > 20:
+  print(2)
+else:
+  print(1)
