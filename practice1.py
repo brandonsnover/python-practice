a3daddb0aa0858863717971a70e39ef7c0548106
brandonsnover <144728738+brandonsnover@users.noreply.github.com>
2024-01-10 13:56:00 -0600

    python practice

--- practice1.py
+++ practice1.py
@@ -14,3 +14,29 @@ print(number + number2)
 # Write a program that uses a variable to store a word, then prints that word in reverse order.
 word = "Hello"
 print(word[::-1])
+
+# Write a program that uses a variable to store a number, then prints the number times 10.
+number = 7
+print (number * 10)
+
+# Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+word1 = "hello"
+word2 = "world"
+print(word1.upper() + word2.upper())
+
+# Write a program that uses a variable to store a word, then prints the number of letters in the word.
+word = "hello"
+print(len(word))
+
+# Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
+number = 12.45
+print(int(number))
+
+# Write a program that uses a variable to store two numbers, then prints the two numbers multiplied together.
+number1 = 7
+number2 = 4
+print(number1 * number2)
+
+# Write a program that uses a variable to store a word, then prints the word with all lowercase letters.
+word = "Hello"
+print(word.lower())
